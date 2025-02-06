5d881d7d7fdf7ec33c0c681e9e496853a491fb2e
Dio Lemos <pedro.devsc@gmail.com>
2025-02-06 15:38:30 -0300

    first commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+
+
+export default function SideBar(){
+
+    return (<aside className="w-1/3 px-8 py-16 bg-teal-500 text-black md:w-72 h-full " >
+<h2 className="text-lg" >Projects</h2>
+<div>
+    <button>+</button>
+</div>
+
+        
+
+    </aside>)
+
+
+}
