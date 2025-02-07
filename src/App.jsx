1830599583ba505e119beb6fd92212c1c57f6008
Dio Lemos <pedro.devsc@gmail.com>
2025-02-07 03:11:14 -0300

    Create NoProjectSelected.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import NewProject from './components/NewProject'
+import NoProjectSelected from './components/NoProjectSelected'
 import SideBar from './components/SideBar'
 
 function App() {
  return(<main className=' bg-[#EFE9D5] h-screen my-8 flex gap-8' >
   <SideBar/>
-  <NewProject/>
+  {/* <NewProject/> */}
+  <NoProjectSelected/>
  </main>)
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,11 @@
+import Button from "./Button";
+
 export default function SideBar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-teal-600 text-green-900 md:w-72 h-full ">
       <h2 className="mb-8 font-bold uppercase md:text-xl">Projects</h2>
       <div>
-      <button className="cursor-pointer px-6 py-2 rounded-md text-teal-500 hover:text-teal-400 bg-green-900 hover:bg-green-800  text-2xl shadow-lg transition-all duration-150 active:scale-90 active:shadow-md">
-  +
-</button>
+      <Button>+</Button>
       </div>
     </aside>
   );
