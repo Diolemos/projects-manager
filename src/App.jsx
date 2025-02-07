27a5ce849a5b09d364c3a9e04177415d0888e458
Dio Lemos <pedro.devsc@gmail.com>
2025-02-07 02:24:53 -0300

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NewProject from './components/NewProject'
 import SideBar from './components/SideBar'
 
 function App() {
- return(<main className='h-screen my-8 flex gap-8' >
+ return(<main className=' bg-[#EFE9D5] h-screen my-8 flex gap-8' >
   <SideBar/>
   <NewProject/>
  </main>)
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,9 @@
 export default function input({label,textarea,...props}){
+    const styles = "w-full p-1 border-b-2 rounded-sm border-stone-500 bg-stone-100 text-stone-500 focus:outline-none focus:border-stone-800"
     return (
-        <label >
-            {label}
-           { textarea ? <textarea {...props} /> : <input {...props} />}
-        </label>
+        <div className="{}flex flex-col gap-1 my-4" >
+          <label className="text-sm font-bold uppercase text-stone-500"> {label}</label> 
+           { textarea ? <textarea className={styles} {...props} /> : <input className={styles} {...props} />}
+        </div>
     )
 }
