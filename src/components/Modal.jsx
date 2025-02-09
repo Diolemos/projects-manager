8f4c288cd75ce3dbdbcbe091bd124f803303f237
diolemos <pedro.devsc@gamil.com>
2025-02-09 20:01:07 -0300

    update modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,23 @@
-import { useImperativeHandle, useRef } from "react"
-import { createPortal } from "react-dom"
-export default function Modal ({ref,children}){
-    
-    const dialogRef = useRef();
-    useImperativeHandle(ref, ()=>{
-        return {
-            open(){
-                dialogRef.current.showModal();
-            }
-        }
-    });
-    return createPortal(<dialog ref={dialogRef} >{children}
-  
-    <form method="dialog" >
-        <button>close</button>
-    </form>    
-        
-        
+import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+export default function Modal({ ref, children }) {
+  const dialogRef = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialogRef.current.showModal();
+      },
+    };
+  });
+  return createPortal(
+    <dialog ref={dialogRef}  className="backdrop:bg-stone-900/90  rounded-md p-8 fixed inset-0 m-auto" >
+      {children}
+
+      <form method="dialog" className="text-center">
+      <button className= "cursor-pointer mt-8  text-white bg-green-950 p-2 rounded">Close</button>
+      </form>
     </dialog>,
 
-    
-    document.getElementById('modal-root')  
-    )
+    document.getElementById("modal-root")
+  );
 }
