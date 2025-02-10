cfcf3734ed6dda06b7854c0cd8639197dc353773
diolemos <pedro.devsc@gamil.com>
2025-02-09 22:55:37 -0300

    add delete.png and update projectDetails.jsx

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,17 +1,25 @@
-export default function ProjectDetails({project}){
-    const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric'
-    });
-    return <div className="w-[35rem mt-16 ">
-        <header className="pb-4 mb-4 border-b-2 border-stone-300" >
-            <div className="flex items-center justify-between" >
-            <h1 className="text-3xl font-bold text-green-800 mb-2" >{project.title}</h1>
-            <button className="text-green-800 hover:text-green-900" >Delete</button></div>
-            <p className="mb-4 text-stone-400" > {formattedDate}</p>
-            <p className=" text-stone-600 whitespace-pre-wrap" >{project.description}</p>
-        </header>
-        tasks
-    </div>
+import deleteIcon from "/src/assets/delete.svg";
+export default function ProjectDetails({ project }) {
+  const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  return (
+    <div className="w-[35rem] mt-16">
+    <header className="pb-4 mb-4 border-b-2 border-stone-300">
+      {/* This flex container ensures the title is on the left and button on the right */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-3xl font-bold text-green-800 mb-2">
+          {project.title}
+        </h1>
+        <button className="cursor-pointer  transition-transform duration-200 hover:scale-110 active:scale-90">
+  <img src={deleteIcon} className="w-6 h-6 " alt="Delete" />
+</button>      </div>
+      <p className="mb-4 text-stone-400">{formattedDate}</p>
+      <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
+    </header>
+    tasks
+  </div>
+  );
 }
